b379a644eaadcdb5df33fa55881470a4c60c4ba5
unknown <jonathan.moran9348@gmail.com>
2022-12-08 12:32:47 -0300

    Estructura de Front con React-Typescript

---
+++ client/src/component/Card/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <img src="" alt=""/>
+        <h1>NOMBRE DE PRODUCTO</h1>
+        <h2>PRECIO: $XXXX</h2>
+    </div>
+  )
+}
+
+export default Card
